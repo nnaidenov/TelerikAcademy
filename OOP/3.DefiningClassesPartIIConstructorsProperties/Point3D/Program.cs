32cca30aa44d0f993959220462a3499fd01b177b
Nikolay Naidenov <n.naidenov92@gmail.com>
2013-02-25 18:54:02 +0200

    Update OOP/3.DefiningClassesPartIIConstructorsProperties/Point3D/Program.cs

--- OOP/3.DefiningClassesPartIIConstructorsProperties/Point3D/Program.cs
+++ OOP/3.DefiningClassesPartIIConstructorsProperties/Point3D/Program.cs
@@ -10,6 +10,15 @@ namespace Point3D
     {
         static void Main(string[] args)
         {
+            Structure3D firstPoint = new Structure3D(1, 2, 3);
+            Structure3D secondPoint = new Structure3D(0, 8, 1);
+
+            Path firstPath = new Path();
+            firstPath.AddPoint(firstPoint);
+            firstPath.AddPoint(secondPoint);
+
+            PathStorage.SavePaths(firstPath);
+            List<Path> pathList = PathStorage.ReadPaths();
         }
     }
 }

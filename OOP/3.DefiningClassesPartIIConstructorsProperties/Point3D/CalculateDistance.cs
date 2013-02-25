36cfc45cd40ecc3413e27c7a8f78d7b24b403787
nnaidenov <n.naidenov92@gmail.com>
2013-02-25 18:01:14 +0200

    Add Next HW - Point3D

---
+++ OOP/3.DefiningClassesPartIIConstructorsProperties/Point3D/CalculateDistance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point3D
+{
+    public static class Distance
+    {
+        public static double CalculateDistance(Structure3D first, Structure3D second)
+        {
+            double distance = 0;
+            distance = Math.Sqrt(Math.Pow(first.PositionX - second.PositionX, 2) +
+                Math.Pow(first.PositionY - first.PositionY, 2) + Math.Pow(first.PositionZ - first.PositionZ, 2));
+
+            return distance;
+        }
+    }
+}

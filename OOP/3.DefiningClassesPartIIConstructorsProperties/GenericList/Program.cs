a351daac9b775927d90e017b2920ddacdd4e85a8
nnaidenov <n.naidenov92@gmail.com>
2013-02-26 11:47:29 +0200

    Generic<T>

---
+++ OOP/3.DefiningClassesPartIIConstructorsProperties/GenericList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> demo = new GenericList<int>(30);
+        }
+    }
+}

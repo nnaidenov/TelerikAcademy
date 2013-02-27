72c4e7d3dc1d62a8e6e6eb4fd266d57ad3a459bc
nnaidenov <n.naidenov92@gmail.com>
2013-02-27 20:59:07 +0200

    Add Ex Matrix

---
+++ OOP/3.DefiningClassesPartIIConstructorsProperties/MyMatrix/Matrix.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMatrix
+{
+    public class Matrix<T>
+    {
+        //Fields
+        private int rows;
+        private int cols;
+        private T[,] matrix;
+        //END FIELDS
+
+        //Constructors
+        public Matrix(int rows, int cols)
+        {
+            this.Rows = rows;
+            this.Cols = cols;
+            this.matrix = new T[rows, cols];
+        }
+        //END CONSTRUCTORS
+
+        //Properties
+        public int Rows
+        {
+            get { return this.rows; }
+            set
+            {
+                if (value > 0 && value <= int.MaxValue)
+                {
+                    this.rows = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Out Of Range!");
+                }
+            }
+        }
+
+        public int Cols
+        {
+            get { return this.cols; }
+            set
+            {
+                if (value > 0 && value <= int.MaxValue)
+                {
+                    this.cols = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Argument Out Of Range Exception!");
+                }
+            }
+        }
+        //END PROPERTIES
+
+        //Indexer
+        public T this[int row, int col]
+        {
+            get
+            {
+                if (row <= this.rows && col <= this.cols)
+                {
+                    return matrix[row, col];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Index Out Of Range Exception!");
+                }
+            }
+            set
+            {
+                if (row <= this.rows && col <= this.cols)
+                {
+                    this.matrix[row, col] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Index Out Of Range Exception!");
+                }
+            }
+        }
+        //END INDEXER
+
+        //Implement the operators
+        //+
+        public static Matrix<T> operator +(Matrix<T> fMatrix, Matrix<T> sMatrix)
+        {
+            int fMatrixRows = fMatrix.rows;
+            int fMatrixCols = fMatrix.cols;
+            int sMatrixRows = sMatrix.rows;
+            int sMatrixCols = sMatrix.cols;
+            
+            Matrix<T> sumMatrix = new Matrix<T>(fMatrixRows, fMatrixCols);
+
+            if (fMatrixCols == sMatrixCols && fMatrixRows == sMatrixRows)
+            {
+                for (int i = 0; i < fMatrixRows; i++)
+                {
+                    for (int j = 0; j < fMatrixCols; j++)
+                    {
+                        sumMatrix[i, j] = (dynamic)fMatrix[i, j] + (dynamic)sMatrix[i, j];
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+            return sumMatrix;
+        }
+        //END +
+
+        //-
+        public static Matrix<T> operator -(Matrix<T> fMatrix, Matrix<T> sMatrix)
+        {
+            int fMatrixRows = fMatrix.rows;
+            int fMatrixCols = fMatrix.cols;
+            int sMatrixRows = sMatrix.rows;
+            int sMatrixCols = sMatrix.cols;
+
+            Matrix<T> sumMatrix = new Matrix<T>(fMatrixRows, fMatrixCols);
+
+            if (fMatrixCols == sMatrixCols && fMatrixRows == sMatrixRows)
+            {
+                for (int i = 0; i < fMatrixRows; i++)
+                {
+                    for (int j = 0; j < fMatrixCols; j++)
+                    {
+                        sumMatrix[i, j] = (dynamic)fMatrix[i, j] - (dynamic)sMatrix[i, j];
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+            return sumMatrix;
+        }
+        //END -
+
+        //*
+        public static Matrix<T> operator *(Matrix<T> fMatrix, Matrix<T> sMatrix)
+        {
+            int fMatrixRows = fMatrix.rows;
+            int fMatrixCols = fMatrix.cols;
+            int sMatrixRows = sMatrix.rows;
+            int sMatrixCols = sMatrix.cols;
+
+            if (fMatrixCols == sMatrixRows && (fMatrixRows > 0 && sMatrixCols > 0 && fMatrixCols > 0))
+            {
+                Matrix<T> result = new Matrix<T>(fMatrixRows, sMatrixCols);
+
+                for (int i = 0; i < fMatrixRows; i++)
+                {
+                    for (int j = 0; j < sMatrixCols; j++)
+                    {
+                        dynamic sum = 0;
+                        for (int k = 0; k < sMatrixCols+1; k++)
+                        {
+                            sum += (dynamic)fMatrix[i, k] * (dynamic)sMatrix[k, j];
+                        }
+                        result.matrix[i, j] = sum;
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+        //END *
+
+        //True
+
+        public static bool operator true(Matrix<T> matrix)
+        {
+            for (int i = 0; i < matrix.rows; i++)
+            {
+                for (int j = 0; j < matrix.cols; j++)
+                {
+                    if ((dynamic)matrix[i,j] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        //END TRUE
+        //IMPLEMENT THE OPERATORS 
+    }
+}
---
+++ OOP/3.DefiningClassesPartIIConstructorsProperties/MyMatrix/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix<int> myMatrix = new Matrix<int>(4, 3);
+            Matrix<int> myMatrix2 = new Matrix<int>(3, 2);
+
+            myMatrix[0, 0] = 1;
+            myMatrix[0, 1] = -1;
+            myMatrix[0, 2] = 0;
+            myMatrix[1, 0] = 0;
+            myMatrix[1, 1] = 2;
+            myMatrix[1, 2] = 3;
+            myMatrix[2, 0] = 1;
+            myMatrix[2, 1] = -3;
+            myMatrix[2, 2] = 1;
+            myMatrix[3, 0] = 4;
+            myMatrix[3, 1] = 0;
+            myMatrix[3, 2] = 2;
+
+            myMatrix2[0, 0] = 2;
+            myMatrix2[0, 1] = 1;
+            myMatrix2[1, 0] = -1;
+            myMatrix2[1, 1] = 0;
+            myMatrix2[2, 0] = 3;
+            myMatrix2[2, 1] = 1;
+
+            for (int row = 0; row < myMatrix.Rows; row++)
+            {
+                for (int col = 0; col < myMatrix.Cols; col++)
+                {
+                    Console.Write("{0} ", myMatrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int row = 0; row < myMatrix2.Rows; row++)
+            {
+                for (int col = 0; col < myMatrix2.Cols; col++)
+                {
+                    Console.Write("{0} ", myMatrix2[row, col]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Matrix<int> newMat = new Matrix<int>(myMatrix.Rows, myMatrix2.Cols);
+            newMat = myMatrix * myMatrix2;
+            for (int row = 0; row < newMat.Rows; row++)
+            {
+                for (int col = 0; col < newMat.Cols; col++)
+                {
+                    Console.Write("{0} ", newMat[row, col]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            if (newMat)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}

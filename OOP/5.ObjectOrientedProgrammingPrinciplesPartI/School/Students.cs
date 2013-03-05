52476ba2cce2bee97ca6d775807f40a2252c839b
nnaidenov <n.naidenov92@gmail.com>
2013-03-05 23:55:51 +0200

    Upload HW School

---
+++ OOP/5.ObjectOrientedProgrammingPrinciplesPartI/School/Students.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    class Students : Class
+    {
+        private byte number;
+
+        public Students()
+        {
+        }
+
+        public Students(string name, string comment, byte number)
+        {
+            this.Number = number;
+            this.Name = name;
+        }
+
+        public byte Number
+        {
+            get { return this.number; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    this.number = value;
+                }
+            }
+        }
+    }
+}

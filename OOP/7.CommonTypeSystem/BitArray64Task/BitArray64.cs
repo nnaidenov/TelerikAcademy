71c973cee696bf2ba584ce222b2b002d4da64c55
nnaidenov <n.naidenov92@gmail.com>
2013-03-11 12:20:53 +0200

    Upload OOP CommonTypeSystem

---
+++ OOP/7.CommonTypeSystem/BitArray64Task/BitArray64.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitArray64Task
+{
+    public class BitArray : IEnumerable<int>
+    {
+        public ulong BitArray64 { get; set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > 63)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                return (BitArray64 & ((ulong)1 << index)) != 0 ? 1 : 0;
+            }
+            set
+            {
+                if (index < 0 || index > 63)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                if (value != 0 && value != 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+
+                if (value == 1)
+                {
+                    this.BitArray64 = this.BitArray64 | ((ulong)1 << index);
+                }
+                else
+                {
+                    this.BitArray64 = this.BitArray64 & (~((ulong)1 << index));
+                }
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < 64; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new BitArray64Enumerator(BitArray64);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            BitArray arr = obj as BitArray;
+            if ((object)arr == null)
+            {
+                return false;
+            }
+
+            return this.BitArray64 == arr.BitArray64;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.BitArray64.GetHashCode();
+        }
+
+        public static bool operator ==(BitArray first, BitArray second)
+        {
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(BitArray first, BitArray second)
+        {
+            return !(first == second);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 64; i++)
+            {
+                sb.AppendFormat("[{0}] = {1} \n", i, this[i]);
+            }
+            return sb.ToString();
+        }
+
+        private class BitArray64Enumerator : IEnumerator
+        {
+            private int index = -1;
+            private ulong bits;
+
+            public BitArray64Enumerator(ulong bits)
+            {
+                this.bits = bits;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    return (bits & ((ulong)1 << index)) != 0 ? 1 : 0;
+                }
+            }
+
+            public bool MoveNext()
+            {
+                index++;
+                return (index < 64);
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+        }
+    }
+}
---
+++ OOP/7.CommonTypeSystem/BitArray64Task/BitTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace BitArray64Task
+{
+    class BitTest
+    {
+        static void Main()
+        {
+            BitArray bits = new BitArray();
+
+            //Random set of bits
+            for (int i = 0; i < 64; i++)
+            {
+                Random random = new Random();
+                StringBuilder getTime = new StringBuilder();
+                
+                int randomNumber = (int)DateTime.Now.Ticks;
+
+                //Трябва ми време за да се получи добър random
+                for (int j = 0; j < 100000; j++)
+                {
+                    Math.Sqrt(j + 1);
+                }
+                //Край на губенето на време
+
+                if (randomNumber % 2 == 0)
+                {
+                    bits[i] = 1;
+                }
+            }
+
+            Console.WriteLine(bits);
+
+            //Print all Bits
+            foreach (var item in bits)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}

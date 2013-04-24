7a340c0fc6ef488e51e0308f5142b8e48fbe044c
nnaidenov <n.naidenov92@gmail.com>
2013-04-25 01:05:49 +0300

    JavaScript Part II, DomManipulation HW

---
+++ JavaScript/JavaScript Part II/1.DOMManipulation/1.RandomDiv/script.js
@@ -0,0 +1,42 @@
+﻿function generateDivs() {
+    var wrapper = document.getElementById("wrapper");
+    var count = document.getElementById("numberDivs").value | 0;
+
+    for (var i = 0; i < count; i++) {
+        var div = document.createElement("div");
+        var strong = document.createElement("strong");
+        strong.innerHTML = strong.innerHTML + "div"
+
+        div.style.width = randomSize(20, 100);
+        div.style.height = randomSize(20, 100);
+        div.style.borderColor = randomColor();
+        div.style.borderWidth = randomSize(1, 20);
+        div.style.borderStyle = "solid";
+        div.style.borderRadius = randomSize(5, 20);
+        div.style.backgroundColor = randomColor();
+        div.style.color = randomColor();
+
+        div.style.position = "absolute";
+        div.style.top = parseInt(Math.random() * (screen.height - 140)) + "px";
+        div.style.left = parseInt(Math.random() * (screen.width - 140)) + "px";
+        //div.style.border = "3px solid " + randomColor();
+
+        wrapper.appendChild(div);
+        div.appendChild(strong);
+    }
+}
+
+function randomSize(min, max) {
+    var size = Math.floor(Math.random() * (max - min + 1)) + min;
+    return size + "px";
+}
+
+function randomColor() {
+    var red = (Math.random() * 256) | 0;
+    var green = (Math.random() * 256) | 0;
+    var blue = (Math.random() * 256) | 0;
+
+    var color = "rgb(" + red + "," + green + "," + blue + ")";
+
+    return color;
+}
---
+++ JavaScript/JavaScript Part II/1.DOMManipulation/5.TagCloud/script.js
@@ -0,0 +1,50 @@
+﻿var tags = ["cms", "javascript", "js", "ASP.NET MVC", ".net",
+    ".net", "css", "wordpress", "xaml", "js", "http", "web", "web",
+    "asp.net", "asp.net MVC", "ASP.NET MVC", "wp", "wp", "wp",
+    "javascript", "js", "cms", "html", "javascript", "http", "http",
+    "http", "http", "CMS", "CMS", "CMS"];
+
+function generateTagCloud(tags, min, max) {
+    var smallTags = new Array();
+    for (var i = 0, l = tags.length; i < l; i+=1) {
+        smallTags[i] = tags[i].toLowerCase();
+    }
+
+    var newTagArr = new Array();
+    var counter = new Array();
+    for (var k = 0; k < smallTags.length; k++) {
+        counter[k] = 0;
+    }
+    
+    for (var i = 0; i < smallTags.length; i++) {
+        var isReplace = false;
+        for (var j = 0; j < newTagArr.length; j++) {
+            if(newTagArr[j] == smallTags[i]) {
+                counter[j] += 1;
+                isReplace = true;
+            }
+        }
+        if (!isReplace) {
+            newTagArr.push(smallTags[i]);
+        }
+    }
+
+    var maxReplace = Math.max.apply(Math, counter);
+    var minReplace = Math.min.apply(Math, counter);
+    var countTags = newTagArr.length;
+    var sizer = Math.floor((max - min) / (maxReplace - minReplace))
+
+    var docFragment = document.createDocumentFragment();
+    for (var i = 0; i < newTagArr.length; i++) {
+        var actualSize = min + sizer * counter[i];
+        var p = document.createElement("p");
+        p.className = "cloud";
+        p.style.fontSize = actualSize + "px";
+        p.innerHTML = newTagArr[i];
+        docFragment.appendChild(p);
+    }
+
+    var wrapper = document.getElementById("wrapper");
+    wrapper.appendChild(docFragment);
+}
+

a4d0c3e6b4fdc236ff9064f078315f143e428520
nnaidenov <n.naidenov92@gmail.com>
2013-04-28 17:29:10 +0300

    Add HQC

---
+++ HQC/3.GoodName/Ex2/Ex2.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex2
+{
+    enum Sex { male, female };
+    class Human
+    {
+        class Person
+        {
+            public Sex sex { get; set; }
+            public string name { get; set; }
+            public int age { get; set; }
+        }
+
+        //Personal Identification Number => PIN
+        public void MakePerson(int PIN)
+        {
+            Person newPerson = new Person();
+            newPerson.age = PIN;
+            if (PIN % 2 == 0)
+            {
+                newPerson.name = "Батката";
+                newPerson.sex = Sex.male;
+            }
+            else
+            {
+                newPerson.name = "Мацето";
+                newPerson.sex = Sex.female;
+            }
+        }
+    }
+}
---
+++ HQC/3.GoodName/Ex4-Minesweeper/GamePoints.cs
@@ -0,0 +1,30 @@
+﻿namespace Minesweeper
+{
+    public class GamePoints
+    {
+        private string name;
+        private int points;
+
+        public GamePoints()
+        {
+        }
+
+        public GamePoints(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        } 
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Points
+        {
+            get { return this.points; }
+            set { this.points = value; }
+        }
+    }
+}
